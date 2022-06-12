05deebd82181be4f222138c358f011146513e2da
Lionsafaritech <nrsingh037@gmail.com>
2022-06-12 14:14:51 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express=require("express");
 const hbs=require("hbs")
 const geocode=require("./utils/geocede")
 const forecast=require("./utils/forecast")
+const port=process.env.PORT ||3000
 //using path module
 // console.log(__dirname)
 // console.log(__filename)
@@ -130,6 +131,6 @@ app.get("*",(req,res)=>{
 // app.get("/weather",(req,res)=>{
 //     res.send("your weather!")
 // })
-app.listen(3000,()=>{
-    console.log("Server is up on port 3000")
+app.listen(port,()=>{
+    console.log("Server is up on port "+port)
 })
